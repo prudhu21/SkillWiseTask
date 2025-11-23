f805c3dfa90a0867f3202dfb95dfe6c90e25e84e
Prudula sri <attuluripavanaprudulasri@gmail.com>
2025-11-23 18:18:57 +0530

    bootstrap dependency added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
